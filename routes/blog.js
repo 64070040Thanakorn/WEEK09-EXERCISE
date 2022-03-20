bdef2eca0db25c0f0f2ba4c72b8891ef315b000b
Bundit Thanasopon <bundittsp@gmail.com>
2022-03-20 23:04:26 +0700

    Update route paths

--- routes/blog.js
+++ routes/blog.js
@@ -4,19 +4,42 @@ const pool = require("../config");
 
 router = express.Router();
 
-router.post("/blogs/search", async function (req, res, next) {
+router.get("/blogs/search", async function (req, res, next) {
   // Your code here
 });
 
 router.post("/blogs/addlike/:blogId", async function (req, res, next) {
+  //ทำการ select ข้อมูล blog ที่มี id = req.params.blogId
+  try{
+    const [rows, fields] = await pool.query("SELECT * FROM blogs WHERE id=?", [
+      req.params.blogId,
+    ]);
+    //ข้อมูล blog ที่เลือกจะอยู่ในตัวแปร rows
+    console.log('Selected blogs =', rows)
+    //สร้างตัวแปรมาเก็บจำนวน like ณ ปัจจุบันของ blog ที่ select มา
+    let likeNum = rows[0].like
+    console.log('Like num =', likeNum) // console.log() จำนวน Like ออกมาดู
+    //เพิ่มจำนวน like ไปอีก 1 ครั้ง
+    likeNum += 1
+
+    //Update จำนวน Like กลับเข้าไปใน DB
+    const [rows2, fields2] = await pool.query("UPDATE blogs SET blogs.like=? WHERE blogs.id=?", [
+      likeNum, req.params.blogId,
+    ]);
+    //Redirect ไปที่หน้า index เพื่อแสดงข้อมูล
+    res.redirect('/');
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// For inserting new blog
+router.post("/create", async function (req, res, next) {
   // Your code here
 });
 
-router.post("/blogs", async function (req, res, next) {
-  // Your code here
-});
 
-router.get("/blogs/:id", function (req, res, next) {
+router.get("/update/:blogId", function (req, res, next) {
   const promise1 = pool.query("SELECT * FROM blogs WHERE id=?", [
     req.params.id,
   ]);
@@ -39,11 +62,13 @@ router.get("/blogs/:id", function (req, res, next) {
     });
 });
 
-router.put("/blogs/:id", function (req, res) {
+// For updating blog
+router.put("/delete/:blogId", function (req, res) {
   // Your code here
   return;
 });
 
+// For deleting blog
 router.delete("/blogs/:id", function (req, res) {
   // Your code here
   return;
